2cd088d837367b54791802234343d8c6f0cea475
Nafis Khan Chowdhury <nafisbuet1@gmail.com>
2021-06-07 22:55:29 -0700

    first commit

---
+++ Week1/1. S2 - UMPIRE_Practice.py
@@ -0,0 +1,13 @@
+# Bismillahir Rahmanir Rahim
+# Session 2 - UMPIRE Practice
+
+# Write a function that reverses a string.
+# Example:
+# Input: "hello"
+# Output: "olleh"
+
+
+
+userInput = input("Please enter a string: ")
+userInput = userInput[::-1]
+print(userInput)
